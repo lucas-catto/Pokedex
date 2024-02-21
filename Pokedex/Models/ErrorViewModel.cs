1c592dbb12bdae3071ce688e8e0850925ae6f082
lucas-catto <lucascattosilva2018@gmail.com>
2024-02-20 22:26:59 -0300

    Criada a Classe Pokemon.

--- Pokedex/Models/ErrorViewModel.cs
+++ Pokedex/Models/ErrorViewModel.cs
@@ -1,8 +1,8 @@
 namespace Pokedex.Models;
 
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
+public class ErrorViewModel {
+    
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
