3f47a27a2bd7514da82ef456e30f477716363056
lucas-catto <lucascattosilva2018@gmail.com>
2024-03-06 21:59:02 -0300

    Criado o javascript para filtrar os Pokemons no Index.cshtml.

--- Pokedex/wwwroot/js/site.js
+++ Pokedex/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function filter (type) {
+    
+    let cards;
+    let i;
+    let count;
+
+    cards   = document.getElementsByClassName("card");
+    buttons = document.getElementsByClassName("btn-filter");
+
+    for (i = 0; i < cards.length; i++) {
+
+        cards[i].parentElement.style.display = 'none';
+
+        if (cards[i].classList.contains(type) || type == "all") {
+            cards[i].parentElement.style.display = 'block';
+            count++;
+        }
+    }
+    for (i = 0; i < buttons.length; i++) {
+        if (buttons[i].id == `btn-${type}`) {
+            buttons.classList.remove("btn-sm");
+            buttons.classList.add("btn-md");
+        }
+        else {
+            buttons.classList.remove("btn-md");
+            buttons.classList.add("btn-sm");
+        }
+    }
+
+    if (type === "all") {
+        document.getElementById("btn-all").classList.remove("btn-sm");
+        document.getElementById("btn-all").classList.add("btn-md");
+    }
+    if (count == 0) {
+        document.getElementById("zeroPokemon").classList.remove("d-none");
+    }
+    else {
+        document.getElementById("zeroPokemon").classList.add("d-none");
+    }
+}
