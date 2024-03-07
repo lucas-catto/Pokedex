9c14b855a0549c14634d70d1595a607071f40987
lucas-catto <lucascattosilva2018@gmail.com>
2024-03-06 21:47:32 -0300

    Refatorado o Código no HomeController.

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -31,50 +31,67 @@ public class HomeController : Controller {
 
         // return View(raichu);
 
-        List<Pokemon> pokemons = [];
+        // List<Pokemon> pokemons = [];
 
-        using(StreamReader leitor = new("Data\\pokemons.json")) {
+        // using(StreamReader leitor = new("Data\\pokemons.json")) {
             
-            string dados = leitor.ReadToEnd();
-            pokemons     = JsonSerializer.Deserialize <List<Pokemon>>(dados);
-        }
+        //     string dados = leitor.ReadToEnd();
+        //     pokemons     = JsonSerializer.Deserialize <List<Pokemon>>(dados);
+        // }
 
-        List<Tipo> tipos = [];
+        // List<Tipo> tipos = [];
 
-        using (StreamReader leitor = new("Data\\tipos.json")) {
+        // using (StreamReader leitor = new("Data\\tipos.json")) {
             
-            string dados = leitor.ReadToEnd();
-            tipos = JsonSerializer.Deserialize <List<Tipo>>(dados);
-        }
+        //     string dados = leitor.ReadToEnd();
+        //     tipos = JsonSerializer.Deserialize <List<Tipo>>(dados);
+        // }
+
+        // ViewData["Tipos"] = tipos;
+
+        // return View(pokemons);
+
+        List<Pokemon> pokemons = GetPokemons();
+        List<Tipo>    tipos    = GetTipos();
 
         ViewData["Tipos"] = tipos;
-
+        
         return View(pokemons);
     }
 
     public IActionResult Details (int id) {
 
-        List<Pokemon> pokemons = [];
+        // List<Pokemon> pokemons = [];
 
-        using (StreamReader leitor = new ("Data\\pokemons.json")) {
+        // using (StreamReader leitor = new ("Data\\pokemons.json")) {
             
-            string dados = leitor.ReadToEnd();
-            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
-        }
+        //     string dados = leitor.ReadToEnd();
+        //     pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        // }
 
-        List<Tipo> tipos = [];
+        // List<Tipo> tipos = [];
 
-        using (StreamReader leitor = new ("Data\\tipos.json")) {
+        // using (StreamReader leitor = new ("Data\\tipos.json")) {
             
-            string dados = leitor.ReadToEnd();
-            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
-        }
+        //     string dados = leitor.ReadToEnd();
+        //     tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        // }
 
-        ViewData["Tipos"] = tipos;
+        // ViewData["Tipos"] = tipos;
 
-        var pokemon = pokemons
-            .Where(p => p.Numero == id)
-            .FirstOrDefault();
+        // var pokemon = pokemons
+        //     .Where(p => p.Numero == id)
+        //     .FirstOrDefault();
+
+        // DetailsVM details = new () {
+        //     Tipos    = tipos,
+        //     Atual    = pokemons.FirstOrDefault(p => p.Numero == id),
+        //     Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
+        //     Proximo  = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),
+        // };
+
+        List<Pokemon> pokemons = GetPokemons();
+        List<Tipo>    tipos    = GetTipos();
 
         DetailsVM details = new () {
             Tipos    = tipos,
@@ -84,6 +101,22 @@ public class HomeController : Controller {
         };
         
         return View(details);
+    }
+
+    private List<Pokemon> GetPokemons () {
+        using (StreamReader leitor = new ("Data\\pokemons.json")) {
+            
+             string dados = leitor.ReadToEnd();
+            return JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+    }
+
+    private List<Tipo> GetTipos () {
+        using (StreamReader leitor = new ("Data\\tipos.json")) {
+            
+            string dados = leitor.ReadToEnd();
+            return JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
